9ed68a0d775197dd65677f16984a10c75503386e
kelvin111027 <kaikang@umich.edu>
2018-09-22 15:47:35 -0400

    add_returnAnswer

--- magic_eight.py
+++ magic_eight.py
@@ -1,3 +1,12 @@
 def ask_question():
 	question = str(input("What is your question? \n(Please end your quesiton with \"?\".\n Type \"quit\" to quit this step.)\n"))
 	return question
+	
+def returnAnswer():
+    possibleAnswers = ["It is certain", "It is decidely so", "Without a doubt", "Yes - definitely",
+    "You may rely on it", "As I see it, yes", "Most likely", "Outlook good", "Yes", "Signs point to yes",
+    "Reply hazy, try again", "Ask again later", "Better not tell you now", "Cannot predict now",
+    "Concentrate and ask again", "Don't count on it", "My reply is no", "My sources say no",
+    "Outlook not so good", "Very doubtful"]
+    Answer = possibleAnswers[random.randint(0,19)]
+    return Answer
