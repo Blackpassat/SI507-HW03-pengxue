de392956116bed23d6336d9fbb96b1c3c589fc00
RaymondXue <raymond@RaymondXues-MacBook-Pro.local>
2018-09-22 15:51:52 -0400

    Created function to check question and runs the program until user inputs quit

--- magic_eight.py
+++ magic_eight.py
@@ -1,3 +1,17 @@
 def ask_question():
 	question = str(input("What is your question? \n(Please end your quesiton with \"?\".\n Type \"quit\" to quit this step.)\n"))
 	return question
+
+def check_question(question):
+	if (question[-1] == '?'):
+		return True
+	else:
+		return False
+
+question = ask_question()
+while question != 'quit':
+	if check_question(question):
+		print("This is a question")
+	else:
+		print("I'm sorry, I can only answer questions. (Please end your question with \"?\")")
+	question = str(input("What is your question?\n"))
