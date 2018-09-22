6b0a3391f565f31d0186ebb864cec9a76d33c881
RaymondXue <raymond@RaymondXues-MacBook-Pro.local>
2018-09-22 16:06:48 -0400

    debug the program

--- magic_eight.py
+++ magic_eight.py
@@ -1,3 +1,4 @@
+import random
 def ask_question():
 	question = str(input("What is your question? \n(Please end your quesiton with \"?\".\n Type \"quit\" to quit this step.)\n"))
 	return question
@@ -20,7 +21,7 @@ def returnAnswer():
 question = ask_question()
 while question != 'quit':
 	if check_question(question):
-		print("This is a question")
+		print(returnAnswer())
 	else:
 		print("I'm sorry, I can only answer questions. (Please end your question with \"?\")")
 	question = str(input("What is your question?\n"))
